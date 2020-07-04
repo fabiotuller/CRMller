6bb4c67560c8504ef65262a6b82046a8029fc593
Fábio Tuller <fabiotuller@gmail.com>
2020-07-04 06:44:27 -0300

    add modal import lead

---
+++ resources/views/admin/leads/modal/import.blade.php
@@ -0,0 +1,22 @@
+<div id="modalImport" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Importar</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            <div class="modal-body">
+                <form action="{{route(//'admin.adm01.citiesImport')}}" method="post" enctype="multipart/form-data" class="btn btn-light">
+                    <label>Selecione um arquivo: </label>
+                    <input type="file" name="file">
+                    @csrf
+                    <input class="btn btn-info" type="submit" value="Upload">
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn-sm btn btn-light" data-dismiss="modal">Cancelar</button>
+            </div>
+        </div>
+
+    </div>
+</div>
